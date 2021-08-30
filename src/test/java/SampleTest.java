eff2f72863bc37aa7972e496188e089eba7d2381
Mohammed Kamle <mohammed@Mohammeds-MacBook-Pro.local>
2021-08-30 17:30:06 +0530

    first commit

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,105 @@
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+public class SampleTest {
+    public static final String AUTOMATE_USERNAME = "YOUR_USERNAME";
+    public static final String AUTOMATE_ACCESS_KEY = "YOUR_ACCESSKEY";
+    public static final String URL = "https://" + AUTOMATE_USERNAME + ":" + AUTOMATE_ACCESS_KEY + "@hub-cloud.browserstack.com/wd/hub";
+
+    public static void main(String[] args) throws MalformedURLException, InterruptedException {
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+
+        // Set URL of the application under test
+        caps.setCapability("app", "YOUR_APPID");
+
+        // Specify device and os_version for testing
+        caps.setCapability("device", "Google Pixel 3");
+        caps.setCapability("os_version", "9.0");
+
+        // Set other BrowserStack capabilities
+        caps.setCapability("project", "First Java Project");
+        caps.setCapability("build", "Java Android");
+        caps.setCapability("name", "first_test");
+
+
+        // Initialise the remote Webdriver using BrowserStack remote URL
+        // and desired capabilities defined above
+        AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(
+                new URL(URL), caps);
+
+
+        // Test case for the BrowserStack sample Android app.
+        // If you have uploaded your app, update the test case here.
+        AndroidElement searchElement = (AndroidElement) new WebDriverWait(driver, 30).until(
+                ExpectedConditions.elementToBeClickable(
+                        MobileBy.AccessibilityId("Search Wikipedia")));
+        searchElement.click();
+
+        // Invoking a selenium session inside an appium session
+        seleniumSession();
+
+        //Continuing appium execution
+        AndroidElement insertTextElement = (AndroidElement) new WebDriverWait(driver, 30).until(
+                ExpectedConditions.elementToBeClickable(
+                        MobileBy.id("org.wikipedia.alpha:id/search_src_text")));
+        insertTextElement.sendKeys("BrowserStack");
+        Thread.sleep(5000);
+        List<AndroidElement> allProductsName = driver.findElementsByClassName(
+                "android.widget.TextView");
+        assert (allProductsName.size() > 0);
+
+
+        // Invoke driver.quit() after the test is done to indicate that the test is completed.
+        driver.quit();
+
+    }
+
+    public static void seleniumSession() throws MalformedURLException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("os_version", "10");
+        caps.setCapability("resolution", "1920x1080");
+        caps.setCapability("browser", "Chrome");
+        caps.setCapability("browser_version", "91.0");
+        caps.setCapability("os", "Windows");
+        caps.setCapability("name", "BStack-[Java] Sample Test"); // test name
+        caps.setCapability("build", "BStack Build Number 1"); // CI/CD job or build name
+        WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
+        driver.get("https://www.google.com");
+        WebElement element = driver.findElement(By.name("q"));
+        element.sendKeys("BrowserStack");
+        element.submit();
+        // Setting the status of test as 'passed' or 'failed' based on the condition; if title of the web page contains 'BrowserStack'
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+        try {
+            wait.until(ExpectedConditions.titleContains("BrowserStack"));
+            markTestStatus("passed","Yaay title contains 'BrowserStack'!",driver);
+        }
+        catch(Exception e) {
+            markTestStatus("failed","Naay title does not contain 'BrowserStack'!",driver);
+        }
+        System.out.println(driver.getTitle());
+        driver.quit();
+    }
+
+    // to mark a test session passed or failed
+    public static void markTestStatus(String status, String reason, WebDriver driver) {
+        JavascriptExecutor jse = (JavascriptExecutor)driver;
+        jse.executeScript("browserstack_executor: {\"action\": \"setSessionStatus\", \"arguments\": {\"status\": \""+status+"\", \"reason\": \""+reason+"\"}}");
+    }
+
+}
+
